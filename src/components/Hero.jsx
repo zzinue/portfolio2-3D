8c96b490dc4f514022556ce960d56ea6be489d0f
zzinue <zzinue@gmail.com>
2024-05-21 09:32:43 -0600

    adding use effect and computers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,22 @@ const Hero = () => {
 
       </div>
         <ComputersCanvas/>
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href="">
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2'>
+              <motion.dev 
+              animate={{
+                y:[0,24, 0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              />
+            </div>
+          </a>
+        </div>
 
     </section>
   )
